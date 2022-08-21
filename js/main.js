6f4549b206b1e6e2e61c8141c9c9296edfb2043a
Rafal Owczarz <r.j.owczarz@gmail.com>
2022-08-21 15:46:57 +0200

    improve accessibility and bugfix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 const burger = document.querySelector('.nav__burger');
 const menu = document.querySelector('.nav__list');
+const navItems = document.querySelectorAll('.nav__list-item');
 
 burger.addEventListener('click', () => {
   menu.classList.toggle('active');
   burger.classList.toggle('active');
+});
+
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    burger.classList.remove('active');
+    menu.classList.remove('active');
+  });
 });
